53f155f1cdfc753192e0b1206c8e52f1e584c329
Hrishik Thakur <91936864+hrishikthakur@users.noreply.github.com>
2022-09-12 18:47:00 +0530

    MOORE VOTING ALGORITHM
    https://leetcode.com/problems/majority-element/

--- 15. Majority element (more than N|2 elements).java
+++ 15. Majority element (more than N|2 elements).java
@@ -1,3 +1,24 @@
+// Using HashMap
+class Solution {
+    public int majorityElement(int[] nums) {
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        for(int i = 0; i < nums.length; i++){
+            hm.put(nums[i], hm.getOrDefault(nums[i], 0) + 1);
+        }
+        
+        int element = 0;
+        int count = 0;
+        for(int key : hm.keySet()){
+            if(hm.get(key) > count){
+                count = hm.get(key);
+                element = key;
+            }
+        }
+        
+        return element;
+    }
+}
+
 // MOORE VOTING ALGORITHM
 class Solution {
     public int majorityElement(int[] nums) {
