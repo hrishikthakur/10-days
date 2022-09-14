d125d58c9e26a4a54ffccabf87e7e6532f08f157
Hrishik Thakur <91936864+hrishikthakur@users.noreply.github.com>
2022-09-14 07:37:53 +0530

    Revise MergeSort
    https://www.codingninjas.com/codestudio/problems/615?topList=striver-sde-sheet-problems&utm_source=striver&utm_medium=website&leftPanelTab=0

--- 12. Count Inversions.java
+++ 12. Count Inversions.java
@@ -1,3 +1,4 @@
+//
 import java.util.* ;
 import java.io.*; 
 public class Solution {
@@ -45,5 +46,45 @@ public class Solution {
         for(int i = 0; i < n; i++)
             cpy[i] = arr[i];
         return mergeSort(cpy, 0, n - 1);
+    }
+}
+
+// MERGE SORT -> Time:O(nlogn)
+class Solution
+{
+    public static void merge(int arr[], int l, int mid, int r)
+    {
+         int i = l;
+         int j = mid + 1;
+         int k = l;
+         int[] temp = new int[100000];
+         while(i <= mid && j <= r){
+             if(arr[i] <= arr[j]){
+                 temp[k] = arr[i];
+                 i++;
+             }else{
+                 temp[k] = arr[j];
+                 j++;
+             }
+             k++;
+         }
+         while(i <= mid)
+             temp[k++] = arr[i++];
+        
+         while(j <= r)
+             temp[k++] = arr[j++];
+             
+          for(int z = l; z <= r; z++)
+              arr[z] = temp[z];
+    }
+
+    public static void mergeSort(int arr[], int l, int r)
+    {
+        if(l < r){
+            int mid = (l + r) / 2;
+            mergeSort(arr, l, mid);
+            mergeSort(arr, mid + 1, r);
+            merge(arr, l, mid, r);
+        }
     }
 }
