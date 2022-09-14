074fd901f9e1dc0ef5367a29c6241d18894fbcd6
Hrishik Thakur <91936864+hrishikthakur@users.noreply.github.com>
2022-09-14 21:03:01 +0530

    Merge Sort implementation
    https://www.codingninjas.com/codestudio/problems/615?topList=striver-sde-sheet-problems&utm_source=striver&utm_medium=website&leftPanelTab=0

--- 12.1 Count Inversions.java
+++ 12.1 Count Inversions.java
@@ -1,32 +1,37 @@
-//
 import java.util.* ;
 import java.io.*; 
 public class Solution {
     
-    public static long merge(long[] arr, int l, int mid, int r){
-        int i = l;
-        int j = mid + 1;
-        int k = l;
-        long[] temp = new long[arr.length];
-        long inversions = 0;
-        while(i <= mid && j <= r){
-            if(arr[j] < arr[i]){
-                inversions = inversions + (mid - i) + 1;
-                temp[k++] = arr[j++];
-            }else if(arr[i] <= arr[j]){
-                temp[k++] = arr[i++];
-            }
-        }
+    public static long merge(long arr[], int l, int mid, int r)
+    {
+         int lSize = mid - l + 1;
+         int rSize = r - mid;
+         
+         long[] L = new long[lSize + 1];
+         long[] R = new long[rSize + 1];
+         
+         for(int i = 0; i < lSize; i++)
+            L[i] = arr[i + l];
+         for(int i = 0; i < rSize; i++)
+            R[i] = arr[i + mid + 1];
+         
+         L[lSize] = R[rSize] = Long.MAX_VALUE;   
+         
+         // Merge
+         long inversions = 0;
+         int lIdx = 0;
+         int rIdx = 0;
+         for(int i = l; i <= r; i++){
+             if(L[lIdx] <=  R[rIdx]){
+                 arr[i] = L[lIdx];
+                 lIdx++;
+             }else{
+                 inversions = inversions + ((lSize - 1) - lIdx + 1);
+                 arr[i] = R[rIdx];
+                 rIdx++;
+             }
+         }
         
-        while(i <= mid)
-            temp[k++] = arr[i++];
-        
-        while(j <= r)
-            temp[k++] = arr[j++];
-        
-        for(int z = l; z <= r; z++)
-            arr[z] = temp[z];
-            
         return inversions;
     }
     
@@ -46,45 +51,5 @@ public class Solution {
         for(int i = 0; i < n; i++)
             cpy[i] = arr[i];
         return mergeSort(cpy, 0, n - 1);
-    }
-}
-
-// MERGE SORT -> Time:O(nlogn)
-class Solution
-{
-    public static void merge(int arr[], int l, int mid, int r)
-    {
-         int i = l;
-         int j = mid + 1;
-         int k = l;
-         int[] temp = new int[100000];
-         while(i <= mid && j <= r){
-             if(arr[i] <= arr[j]){
-                 temp[k] = arr[i];
-                 i++;
-             }else{
-                 temp[k] = arr[j];
-                 j++;
-             }
-             k++;
-         }
-         while(i <= mid)
-             temp[k++] = arr[i++];
-        
-         while(j <= r)
-             temp[k++] = arr[j++];
-             
-          for(int z = l; z <= r; z++)
-              arr[z] = temp[z];
-    }
-
-    public static void mergeSort(int arr[], int l, int r)
-    {
-        if(l < r){
-            int mid = (l + r) / 2;
-            mergeSort(arr, l, mid);
-            mergeSort(arr, mid + 1, r);
-            merge(arr, l, mid, r);
-        }
     }
 }
